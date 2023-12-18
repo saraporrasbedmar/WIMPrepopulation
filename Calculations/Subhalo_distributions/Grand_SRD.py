6d22fbb49d7584fcd8521f5f2406979f0bf705b9
aras <saraporrasbedmar@gmail.com>
2023-12-18 15:41:36 +0100

    srd new possible fit: exp cutoff

--- Calculations/Subhalo_distributions/Grand_SRD.py
+++ Calculations/Subhalo_distributions/Grand_SRD.py
@@ -129,7 +129,7 @@ def fragile(R, yy_args, x_cut):
 
 # ---------------------------------------------------------------------------------------------------
 print('Density figure')
-
+'''
 plt.close('all')
 plt.figure(figsize=(12, 7))
 num_bins = 15
@@ -171,6 +171,12 @@ plt.plot(x_med_kpc, (x_med_kpc ** 0.54343928
 plt.plot(x_med_kpc, (x_med_kpc ** 0.97254648
                      * 10 ** -3.08584275) / vol,
          '--', marker='+', ms=10, color='limegreen', alpha=0.7)
+
+plt.plot(x_med_kpc, (0.0711) / vol,
+         '--', marker='*', ms=10,
+         color='grey', alpha=0.7, label='res_maximum N/Ntot')
+plt.plot(x_med_kpc, (0.08394) / vol,
+         '--', marker='*', ms=10, color='limegreen', alpha=0.7)
 
 xmin = 2
 xmax = 11
@@ -349,8 +355,8 @@ ax1.add_artist(legend33)
 plt.xscale('log')
 plt.yscale('log')
 
-plt.savefig('Outputs/srd_num.png', bbox_inches='tight')
-plt.savefig('Outputs/srd_num.pdf', bbox_inches='tight')
+plt.savefig('outputs/srd_num.png', bbox_inches='tight')
+plt.savefig('outputs/srd_num.pdf', bbox_inches='tight')
 
 print()
 print('Place of [Mpc] and number of subs below the resilient-disrupted cut:')
@@ -362,7 +368,7 @@ print('%.5f ----- %r' % (yy_hyd[0][0] * yy_hyd[0][1] / yy_hyd[0][2],
                          np.shape(
                              Grand_hydro[Grand_hydro[:, 2]
                                          <= peak_hyd / 1e3, :])[0]))
-'''
+
 # ----------------------- FIG 3 -----------------------------------------
 fig = plt.figure(3, figsize=(13, 10))
 ax1 = plt.gca()
@@ -694,80 +700,158 @@ plt.yscale('log')
 plt.xlim(6, 300)
 
 # plt.ylim(0, 60)
-
+'''
 # ------------------------ N(r)/Ntot figure ------------------------------
 print()
 print('N/Ntot figures')
 
-plt.figure(figsize=(10, 8))
-plt.title('DMO')
+# plt.figure(figsize=(10, 8))
+# ax1 = plt.gca()
+fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(18, 8))
+plt.subplot(121)
+
+v_cut = [8.0]  # np.linspace(6, 10, num=10)
+cm_subsection = np.linspace(0, 1, 25)
+from matplotlib import cm
+
+colors = [cm.jet(x) for x in cm_subsection]
+
+num_bins = 15
+bins = np.linspace(0, R200, num=num_bins)
+bins_mean = (bins[:-1] + bins[1:]) / 2. * 1e3
+
+xxx = np.linspace(3e-3, R_max, num=200) * 1e3
 
 for ni, ii in enumerate(v_cut):
     print('v_cut: ', ii, ni)
-    print((1))
     Grand_dmo_over = Grand_dmo[Grand_dmo[:, 1] > ii, :]
-    Grand_dmo_below = Grand_dmo[Grand_dmo[:, 1] <= ii, :]
     Grand_hydro_over = Grand_hydro[Grand_hydro[:, 1] > ii, :]
-    Grand_hydro_below = Grand_hydro[Grand_hydro[:, 1] <= ii, :]
 
     srd_dmo_over = (np.array(encontrar_SRD_sinVol(Grand_dmo_over))
                     / len(Grand_dmo_over))
-    srd_dmo_below = (np.array(encontrar_SRD_sinVol(Grand_dmo_below))
-                     / len(Grand_dmo_below))
     srd_hydro_over = (np.array(encontrar_SRD_sinVol(Grand_hydro_over))
                       / len(Grand_hydro_over))
-    srd_hydro_below = (np.array(encontrar_SRD_sinVol(Grand_hydro_below))
-                       / len(Grand_hydro_below))
-    print(encontrar_SRD_sinVol(Grand_dmo_below))
-    print(len(Grand_dmo_below))
-    print(srd_dmo_below)
-    print(Grand_dmo_below[:10, 2])
-    print()
-    print()
 
-    plt.plot(x_med * 1e3, srd_dmo_over, '-', color=colors[ni], #ms=10,
-             alpha=(1), label='%.1f' % ii)
+    plt.plot(bins_mean, srd_dmo_over, '.-', color='k', ms=10,
+             alpha=(1), label='Data')
 
-    # plt.plot(x_med * 1e3, srd_hydro_over, '-', color=colors[ni],# ms=10,
-    #          alpha=(1), label='%.1f' % ii)
-
-    plt.plot(x_med * 1e3, srd_dmo_below, '--', color=colors[ni],# ms=10,
+    plt.plot(bins_mean, srd_hydro_over, '.-', color='limegreen', ms=10,
              alpha=(1))
 
-    # plt.plot(x_med * 1e3, srd_hydro_below, '--', color=colors[ni],# ms=10,
-    #          alpha=(1))
+
+def funct_ale(Dgc, a, b):
+    return b * np.exp(a / Dgc)
 
 
+cts_dmo = opt.curve_fit(funct_ale, xdata=bins_mean,
+                        ydata=srd_dmo_over,
+                        p0=[-20, 0.1])
+print('Funct Ale: ', cts_dmo[0])
 
-srd_dmo = (np.array(encontrar_SRD_sinVol(Grand_dmo))
-                    / len(Grand_dmo))
+plt.plot(bins_mean, np.ones(len(bins_mean)) * cts_dmo[0][1],
+         marker='*', linestyle='dotted', color='k', label='resilient possible')
 
-srd_hydro = (np.array(encontrar_SRD_sinVol(Grand_hydro))
-                      / len(Grand_hydro))
-plt.plot(x_med * 1e3, srd_dmo, '-', color='k', ms=10, label='total',
-         linewidth=5)
-# plt.plot(x_med * 1e3, srd_hydro, '--', color='k', ms=10)
+plt.plot(xxx, funct_ale(xxx, cts_dmo[0][0], cts_dmo[0][1]),
+         'k', linestyle='--', label='exp cutoff fit')
+plt.plot(bins_mean, funct_ale(bins_mean, cts_dmo[0][0], cts_dmo[0][1]),
+         ls='', marker='+', ms=10, color='k', alpha=1)
 
-plt.axvline(peak_dmo, alpha=0.5, color='k')
-plt.axvline(peak_hyd, alpha=0.5, color='limegreen')
+cts_hydro = opt.curve_fit(funct_ale, xdata=bins_mean,
+                        ydata=srd_hydro_over,
+                        p0=[-20, 0.1])
+print('Funct Ale: ', cts_hydro[0])
+
+plt.plot(bins_mean, np.ones(len(bins_mean)) * cts_hydro[0][1],
+         marker='*', linestyle='dotted', color='limegreen')
+
+plt.plot(xxx, funct_ale(xxx, cts_hydro[0][0], cts_hydro[0][1]),
+         'limegreen', linestyle='--')
+plt.plot(bins_mean, funct_ale(bins_mean, cts_hydro[0][0], cts_hydro[0][1]),
+         ls='', marker='+', ms=10, color='limegreen', alpha=1)
 
 plt.axvline(R_max * 1e3, alpha=0.5, label='220 kpc')
 
 plt.ylabel(r'n(r) = $\frac{N(r)}{N_{Tot}}$')
 plt.xlabel('r [kpc]', size=24)
 
+plt.xscale('linear')
+plt.yscale('log')
+
+plt.ylim(1e-3, 2e-1)
+
+# plt.legend(framealpha=1, fontsize=10, loc=4)
+
+
+legend11 = plt.legend(loc=4, framealpha=1)
+
+# colors = ['k', 'limegreen']
+# legend33 = plt.legend([plt.Line2D([], [],
+#                                   linestyle='', marker='o',
+#                                   color=colors[i])
+#                        for i in range(2)],
+#                       ['DMO', 'Hydro'],
+#                       loc=8, title='Simulation', framealpha=1,
+#                       # bbox_to_anchor=(0.99, 0.6)
+#                       )
+
+ax1.add_artist(legend11)
+# ax1.add_artist(legend33)
+
+# -------------------------------------------------------------------------
+print('Density figure')
+
+# plt.figure(figsize=(10, 8))
+plt.subplot(122)
+srd_dmo = np.array(encontrar_SRD(Grand_dmo_over)) / len(Grand_dmo_over)
+srd_hydro = np.array(encontrar_SRD(Grand_hydro_over)) / len(Grand_hydro_over)
+
+plt.plot(bins_mean, srd_dmo, '.-', color='k', ms=10, label='Data')
+plt.plot(bins_mean, srd_hydro, '.-', color='limegreen', ms=10)
+
+xxx = np.linspace(3e-3, R200 * 1e3, num=100)
+
+vol = 4 / 3 * np.pi * (bins[1:] ** 3 - bins[:-1] ** 3)
+
+print((bins[:-1] - bins[1:]))
+print(cts_hydro[0][1] / vol/(cts_dmo[0][1] / vol))
+plt.plot(bins_mean, cts_dmo[0][1] / vol,# * (bins[1:] - bins[:-1])*1e3,
+         '--', marker='*', ms=10,
+         color='grey', alpha=1, linestyle='dotted', label='resilient possible')
+plt.plot(bins_mean, cts_hydro[0][1] / vol,
+         '--', marker='*', linestyle='dotted', ms=10, color='limegreen', alpha=1)
+
+plt.plot(bins_mean, funct_ale(bins_mean, cts_dmo[0][0], cts_dmo[0][1]) / vol,
+         '--', marker='+', ms=10,
+         color='grey', alpha=1, label='exp cutoff')
+plt.plot(bins_mean, funct_ale(bins_mean, cts_hydro[0][0], cts_hydro[0][1]) / vol,
+         '--', marker='+', ms=10, color='limegreen', alpha=1)
+
+print(cts_dmo, cts_hydro)
+
+
+plt.axvline(R_max * 1e3, alpha=0.7, linestyle='--')  # , label='220 kpc')
+plt.annotate(r'R$_\mathrm{vir}$', (170, 32), color='b', rotation=45, alpha=0.7)
+
+plt.axvline(8.5, linestyle='--', alpha=1, color='Sandybrown')
+plt.annotate('Earth', (8.6, 35), color='Sandybrown', rotation=45)
+
+plt.ylabel(r'n(r) = $\frac{N(r)}{N_{Tot}\,Volumen}$ [Mpc$^{-3}$]', size=24)
+plt.xlabel('r [kpc]', size=26)
+
+legend_elements = [Line2D([0], [0], marker='o', color='w', label='Frag',
+                          markerfacecolor='k', markersize=8),
+                   Line2D([0], [0], marker='o', color='w', label='Res',
+                          markerfacecolor='limegreen', markersize=8)]
+legend1 = plt.legend(legend_elements, ['DMO', 'Hydro'], loc=8,
+                     handletextpad=0.2)  # ,handlelength=1)
+leg = plt.legend(framealpha=1, loc=1)
+plt.gca().add_artist(legend1)
 
 plt.xscale('log')
 plt.yscale('log')
+# plt.xlim(6, 300)
 
-legend_elements = [Line2D([0], [0], color='k',
-                           markersize=8),
-                   Line2D([0], [0], color='k', ls='--',
-                          markersize=8)]
-legend1 = plt.legend(legend_elements, ['Over', 'Below'], loc=8,
-                     handletextpad=0.2)  # ,handlelength=1)
+# plt.ylim(0, 60)
 
-plt.legend(framealpha=1, fontsize=10, loc=4)
-plt.gca().add_artist(legend1)
-'''
+plt.savefig('outputs/srd_compar_log.png')
 plt.show()
